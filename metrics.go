30912d0827c7ffa3ad1e8734fa946abe158c4e4a
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-30 16:22:36 -0300

    Refactor metrics package: Remove unused const.go and otlp.go files, add new HTTP and system metrics collectors, and implement basic metrics collection. Introduce stdout and noop packages for future metrics handling. Update metrics.go to streamline the structure and remove deprecated code.

--- metrics.go
+++ metrics.go
@@ -3,31 +3,3 @@
 // All rights reserved.
 
 package metrics
-
-import (
-	"github.com/goxkit/configs"
-	"github.com/goxkit/logging"
-)
-
-type (
-	// Headers is a map of string key-value pairs used for metric exporter HTTP headers.
-	Headers map[string]string
-
-	// MetricExporterType defines the type of metrics exporter being used.
-	MetricExporterType int32
-
-	// OTLPCompression defines the compression type used for OpenTelemetry Protocol.
-	OTLPCompression string
-
-	// basicMetricsAttr provides common attributes needed by metrics exporters.
-	basicMetricsAttr struct {
-		// logger is used for logging metrics-related events and errors.
-		logger logging.Logger
-
-		// cfg contains application configuration values.
-		cfg *configs.Configs
-
-		// appName stores the application name for metrics identification.
-		appName string
-	}
-)
