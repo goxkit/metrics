0a915012e2d2a334051f58cea635271b288152e7
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-31 09:05:43 -0300

    feat(metrics): implement Install function for OTLP and noop metrics

--- metrics.go
+++ metrics.go
@@ -3,3 +3,18 @@
 // All rights reserved.
 
 package metrics
+
+import (
+	"github.com/goxkit/configs"
+	"github.com/goxkit/metrics/noop"
+	"github.com/goxkit/metrics/otlp"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+)
+
+func Install(cfgs *configs.Configs) (*sdkmetric.MeterProvider, error) {
+	if cfgs.OTLPConfigs.Enabled {
+		return otlp.Install(cfgs)
+	}
+
+	return noop.Install(cfgs)
+}
